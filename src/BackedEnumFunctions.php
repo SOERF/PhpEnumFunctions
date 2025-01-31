2ce8290c053486fd1de9f771d30dc010acfe8616
stefan <skaesdorf@soerf.de>
2025-01-31 13:02:59 +0100

    Initial commit

---
+++ src/BackedEnumFunctions.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * @property string value
+ * @method static from()
+ * @method static tryFrom()
+ */
+trait BackedEnumFunctions
+{
+    use EnumFunctions;
+
+    public static function fromValue ( string|int $value ) : static
+    {
+        return static::from( $value );
+    }
+
+    public static function hasValue ( string|int $value ) : bool
+    {
+        return !!static::tryFrom( $value );
+    }
+
+    public static function getValues() : array
+    {
+        return collect(static::cases())->map(fn(BackedEnum $c)=>$c->value)->all();
+    }
+
+    public function serializedValue () : string|int
+    {
+        return $this->value;
+    }
+}
---
+++ src/EnumFunctions.php
@@ -0,0 +1,89 @@
+<?php
+
+use Illuminate\Support\Collection;
+use Illuminate\Support\Enumerable;
+
+/**
+ * @property string name
+ * @method static cases()
+ */
+trait EnumFunctions
+{
+    public function is ( mixed $enum ) : bool
+    {
+        if ( $enum instanceof static )
+        {
+            return $this === $enum;
+        }
+        return $this->serializedValue() === $enum;
+    }
+
+    public function isNot ( mixed $enum ) : bool
+    {
+        return !$this->is( $enum );
+    }
+
+    public function in ( array|Collection|Enumerable $enums ) : bool
+    {
+        foreach ( $enums as $enum )
+        {
+            if ( $this->is( $enum ) )
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public function notIn ( array|Collection|Enumerable $enums ) : bool
+    {
+        return !$this->in( $enums );
+    }
+
+    public static function fromKey ( string $key ) : static
+    {
+        return static::fromName( $key );
+    }
+
+    public static function fromName ( ?string $name ) : ?static
+    {
+        if ( $name === null )
+        {
+            return null;
+        }
+        return static::class::{$name};
+    }
+
+    public static function getInstances () : array
+    {
+        return static::cases();
+    }
+
+    public static function hasName ( string $name ) : bool
+    {
+        try
+        {
+            static::fromName( $name );
+            return true;
+        }
+        catch ( Error $e )
+        {
+            return false;
+        }
+    }
+
+    public static function getNames () : array
+    {
+        return collect( static::cases() )->map( fn( UnitEnum $c ) => $c->name )->all();
+    }
+
+    public static function count () : int
+    {
+        return count( static::cases() );
+    }
+
+    public function serializedValue () : string
+    {
+        return $this->name;
+    }
+}
